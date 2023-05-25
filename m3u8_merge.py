65b94f5c2fb9528beb117dfe3f198c03bcff74fa
Windham Wong <windhamwong@nva-hk.com>
2023-05-25 11:49:22 +0800

    Update m3u8_merge.py

--- m3u8_merge.py
+++ m3u8_merge.py
@@ -5,15 +5,25 @@ def main(args):
   prefix = filelist[0].split('-')[0]
   postfix = '-'.join(filelist[0].split('-')[2:])
 
-  output = '%s/%s' % (args.path, args.name)
+  output = '%s/temp.mp4' % (args.path)
+  try:
+    os.remove(output)
+  except:
+    next
+
   with open(output, 'ab+') as f:
     for i in range(1, len(filelist)+1):
+      if not os.path.exists('%s/%s-%s-%s' % (args.path, prefix, str(i), postfix)):
+        print("Missing file %s-%s-%s" % (prefix, str(i), postfix))
+        continue
       with open('%s/%s-%s-%s' % (args.path, prefix, str(i), postfix), 'rb') as f2:
         f.write(f2.read())
+
+  os.system('ffmpeg -i %s -c copy %s/%s' % (output, args.path, args.name))
   print('[*] Done.')
 
+  os.remove(output)
 
-  print(args)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
